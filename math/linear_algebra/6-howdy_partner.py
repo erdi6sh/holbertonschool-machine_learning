0b93c99c81cfe343364f6f487bc7c9fb9a4adcaa
erdi6sh <erdishpati@icloud.com>
2025-10-18 12:59:24 +0000

    Concating two arrays

--- math/linear_algebra/6-howdy_partner.py
+++ math/linear_algebra/6-howdy_partner.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-"""This module containes a function that concatenates two arrays"""
+"""This module contains a function that concatenates two arrays."""
 
 
 def cat_arrays(arr1, arr2):
-    """Function that adds two arrays together and stores them in a new array."""
+    """Concatenates two arrays and returns a new array."""
     catted_array = arr1 + arr2
     return catted_array
