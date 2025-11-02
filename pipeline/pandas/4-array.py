b3b7d9bb9035e6eac235f0b60b769e9c75cc9bcc
erdi6sh <erdishpati@icloud.com>
2025-11-02 23:17:04 +0000

    pycodestyle fix

--- pipeline/pandas/4-array.py
+++ pipeline/pandas/4-array.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Module that converts last 10 rows of High and Close columns to ndarray."""
 
+
 def array(df):
     """
     Selects the last 10 rows of 'High' and 'Close' columns from a DataFrame
