b1381a040e88d87d06adc7abd1ff25d79116a299
erdi6sh <erdishpati@icloud.com>
2025-11-02 23:16:01 +0000

    import fix

--- pipeline/pandas/4-array.py
+++ pipeline/pandas/4-array.py
@@ -1,19 +1,15 @@
 #!/usr/bin/env python3
-"""Module to convert last 10 rows of selected columns to a NumPy array."""
-
-import pandas as pd
-import numpy as np
-
+"""Module that converts last 10 rows of High and Close columns to ndarray."""
 
 def array(df):
     """
     Selects the last 10 rows of 'High' and 'Close' columns from a DataFrame
-    and converts them to a NumPy array.
+    and converts them to a NumPy ndarray.
 
     Args:
-        df (pd.DataFrame): The DataFrame containing 'High' and 'Close' columns.
+        df (pd.DataFrame): DataFrame containing 'High' and 'Close' columns.
 
     Returns:
-        np.ndarray: A NumPy array with the last 10 rows of 'High' and 'Close'.
+        numpy.ndarray: Array containing the last 10 rows of High and Close.
     """
     return df[['High', 'Close']].tail(10).to_numpy()
