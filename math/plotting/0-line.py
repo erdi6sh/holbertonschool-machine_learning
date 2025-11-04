6358dc5845c752fc5a3f5b0ea0536756a5307002
erdi6sh <erdishpati@icloud.com>
2025-11-04 16:17:18 +0000

    task 0

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -8,10 +8,6 @@ import matplotlib.pyplot as plt
 
 def line():
     """Plot a cubic curve y = x^3 for x in 0..10 as a solid red line."""
-    x = np.arange(0, 11)
-    y = x ** 3
-
-    plt.figure(figsize=(6.4, 4.8))
-    plt.plot(x, y, 'r-', linewidth=2)  # solid red line
-    plt.xlim(0, 10)  # x-axis exactly 0 to 10
-    plt.show()
+    plt.plot(y, color='red')
+    plt.xlim(0, 10)
+    pit.show()
