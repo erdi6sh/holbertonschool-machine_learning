a3af89ac86aa56af3cb161730e77f9b35f3e01d1
erdi6sh <erdishpati@icloud.com>
2025-11-04 16:14:16 +0000

    pushing readme

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,21 +1,17 @@
 #!/usr/bin/env python3
 """
-Module that plots a simple line graph.
+Plot y = x^3 as a solid red line with x-axis from 0 to 10.
 """
-
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 def line():
-    """
-    Plots y = x^3 as a solid red line
-    for x values from 0 to 10.
-    """
+    """Plot a cubic curve y = x^3 for x in 0..10 as a solid red line."""
     x = np.arange(0, 11)
     y = x ** 3
 
     plt.figure(figsize=(6.4, 4.8))
-    plt.plot(x, y, color='red', linestyle='-', linewidth=1)
-    plt.xlim(0, 10)
+    plt.plot(x, y, 'r-', linewidth=2)  # solid red line
+    plt.xlim(0, 10)  # x-axis exactly 0 to 10
     plt.show()
