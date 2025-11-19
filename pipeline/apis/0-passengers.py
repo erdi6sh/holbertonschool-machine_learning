b87c93f6fe43cbcb0854496b5deb9ca219673aaf
erdi6sh <erdishpati@icloud.com>
2025-11-19 19:39:43 +0000

    Task 0

--- pipeline/apis/0-passengers.py
+++ pipeline/apis/0-passengers.py
@@ -1,34 +1,39 @@
 #!/usr/bin/env python3
-"""Module to retrieve starships from SWAPI capable of holding a given number of passengers."""
+"""Retrieve starships from SWAPI that can hold a given number of passengers."""
 
 import requests
 
 
 def availableShips(passengerCount):
     """
-    Return a list of starship names that can hold at least passengerCount passengers.
+    Return starship names that can hold at least passengerCount passengers.
 
     Args:
-        passengerCount (int): The minimum number of passengers the starship must hold.
+        passengerCount (int): Minimum number of passengers the starship
+            must hold.
 
     Returns:
-        list: List of starship names (str) meeting the passenger requirement.
+        list: Starship names (str) meeting the passenger requirement.
               Returns an empty list if no ships qualify.
     """
     ships = []
     url = 'https://swapi-api.hbtn.io/api/starships/'
+
     while url:
         res = requests.get(url)
         if res.status_code != 200:
             break
+
         data = res.json()
         for ship in data.get('results', []):
-            passengers = ship.get('passengers', '0').replace(',', '').split()[0]
+            passengers = ship.get('passengers', '0')
+            passengers = passengers.replace(',', '').split()[0]
             try:
                 num = int(passengers)
             except ValueError:
                 continue
             if num >= passengerCount:
-                ships.append(ship['name'])
+                ships.append(ship.get('name'))
         url = data.get('next')
+
     return ships
