08fd9035ddebaf1c2e27139ca58ffae9f9ff81ae
erdi6sh <erdishpati@icloud.com>
2025-12-23 11:54:40 +0000

    Task 12

--- unsupervised_learning/clustering/6-expectation.py
+++ unsupervised_learning/clustering/6-expectation.py
@@ -1,57 +1,44 @@
 #!/usr/bin/env python3
-"""Expectation step in EM algorithm for GMM"""
+""" E-step: expected value of likelihood function
+    respect the conditional distribution
+"""
+
 import numpy as np
 pdf = __import__('5-pdf').pdf
 
 
 def expectation(X, pi, m, S):
-    """
-    Calculates the expectation step in the EM algorithm for a GMM
-
-    Args:
-        X: numpy.ndarray of shape (n, d) containing the data set
-        pi: numpy.ndarray of shape (k,) containing priors for each cluster
-        m: numpy.ndarray of shape (k, d) containing centroid means
-        S: numpy.ndarray of shape (k, d, d) containing covariance matrices
-
-    Returns:
-        g: numpy.ndarray of shape (k, n) containing posterior probabilities
-        l: total log likelihood
-        or None, None on failure
-    """
+    """Function that calculates the expectation step in the EM algorithm for a GMM"""
     if not isinstance(X, np.ndarray) or len(X.shape) != 2:
-        return None, None
-    if not isinstance(pi, np.ndarray) or len(pi.shape) != 1:
         return None, None
     if not isinstance(m, np.ndarray) or len(m.shape) != 2:
         return None, None
+    if not isinstance(pi, np.ndarray) or len(pi.shape) != 1:
+        return None, None
     if not isinstance(S, np.ndarray) or len(S.shape) != 3:
+        return None, None
+    if not np.isclose([np.sum(pi)], [1])[0]:
         return None, None
 
     n, d = X.shape
     k = pi.shape[0]
-
-    if m.shape[0] != k or m.shape[1] != d:
+    if d != m.shape[1] or d != S.shape[1] or d != S.shape[2]:
         return None, None
-    if S.shape[0] != k or S.shape[1] != d or S.shape[2] != d:
-        return None, None
-
-    if not np.isclose(np.sum(pi), 1):
+    if k != m.shape[0] or k != S.shape[0]:
         return None, None
 
-    try:
-        likelihoods = np.zeros((k, n))
+    centroids_mean = m
+    covariance_mat = S
+    gauss_components = np.zeros((k, n))
 
-        for i in range(k):
-            P = pdf(X, m[i], S[i])
-            if P is None:
-                return None, None
-            likelihoods[i] = pi[i] * P
+ 
+ 
+    for i in range(k):
+        likelihood = pdf(X, centroids_mean[i], covariance_mat[i])
+        prior = pi[i]
+        gauss_components[i] = likelihood * prior
+    g = gauss_components / np.sum(gauss_components, axis=0)
 
-        marginal = np.sum(likelihoods, axis=0)
-        g = likelihoods / marginal
-        l = np.sum(np.log(marginal))  # noqa: E741
+    log_likelihood = np.sum(np.log(np.sum(gauss_components, axis=0)))
 
-        return g, l
-    except Exception:
-        return None, None
+    return g, log_likelihood
