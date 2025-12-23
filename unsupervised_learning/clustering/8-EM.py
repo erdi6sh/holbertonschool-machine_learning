cc5224fd2a358b53027a24329dd47c70d8f10499
erdi6sh <erdishpati@icloud.com>
2025-12-23 11:17:41 +0000

    Task 8

--- unsupervised_learning/clustering/8-EM.py
+++ unsupervised_learning/clustering/8-EM.py
@@ -56,6 +56,8 @@ def expectation_maximization(X, k, iterations=1000, tol=1e-5, verbose=False):
                 print(f"Log Likelihood after {i} iterations: {l:.5f}")
 
             if i > 0 and abs(l - l_prev) <= tol:
+                if verbose and i % 10 != 0:
+                    print(f"Log Likelihood after {i} iterations: {l:.5f}")
                 break
 
             l_prev = l
