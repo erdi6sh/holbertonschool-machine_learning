e5f97b10224aa5984aae9a6d62355f888d403b9f
erdi6sh <erdishpati@icloud.com>
2026-01-15 14:16:17 +0000

    task 1

---
+++ supervised_learning/classification/1-neuron.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""Neuron class for binary classification"""
+import numpy as np
+
+
+class Neuron:
+    """Single neuron performing binary classification"""
+
+    def __init__(self, nx):
+        """Initialize neuron with nx input features"""
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """Getter for weights vector"""
+        return self.__W
+
+    @property
+    def b(self):
+        """Getter for bias"""
+        return self.__b
+
+    @property
+    def A(self):
+        """Getter for activated output"""
+        return self.__A
